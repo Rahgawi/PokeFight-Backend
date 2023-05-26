db7467b9ac31a50bed9e48e71391c48c5b882f9d
Salvatore <111086457+salvo081@users.noreply.github.com>
2023-05-26 12:50:03 +0200

    getAllPokemons: added parameters for query a range of array objects

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,16 +1,23 @@
 const pokemon = require('../pokedata/pokedex.json');
-function getAllPokemons(req,res){
-res.status(200).send(pokemon);
-}
-function getPokemonById(req,res){
-    const {id} = req.params;
-    const singlePokemon = pokemon.find((item) => item.id == id);
-res.status(200).send(singlePokemon);
+function getAllPokemons(req, res) {
+  const { indexFrom, indexTo } = req.query;
+
+  if (indexFrom && indexTo) {
+    res.status(200).send(pokemon.slice(indexFrom, indexTo));
+  } else {
+    res.status(200).send(pokemon);
+  }
 }
 
-function getPokemonInfo(req,res){
-    const {info} = req.params;
-res.status(200).send(`get the pokomon info ${info}`);
+function getPokemonById(req, res) {
+  const { id } = req.params;
+  const singlePokemon = pokemon.find((item) => item.id == id);
+  res.status(200).send(singlePokemon);
 }
 
-module.exports = {getAllPokemons,getPokemonById,getPokemonInfo};
+function getPokemonInfo(req, res) {
+  const { info } = req.params;
+  res.status(200).send(`get the pokomon info ${info}`);
+}
+
+module.exports = { getAllPokemons, getPokemonById, getPokemonInfo };
--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 const cors = require('cors');
-const pokemonRouter =  require('./routes/pokemon');
+const pokemonRouter = require('./routes/pokemon');
 
 app.use(cors());
-app.use('/',pokemonRouter);
+app.use('/', pokemonRouter);
 //app.get('/',routes);
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
-  });
+  console.log(`Example app listening on port ${PORT}`);
+});
